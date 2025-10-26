b9053f8f256f35d979ae7e153b308416eef6c38b
ivant <ivan.titov093.093@gmail.com>
2025-10-26 13:24:49 +0300

    FragmentMain

--- app/src/main/java/com/titoshvily/gpstracker/fragments/MainFragment.kt
+++ app/src/main/java/com/titoshvily/gpstracker/fragments/MainFragment.kt
@@ -2,113 +2,166 @@ package com.titoshvily.gpstracker.fragments
 
 import android.Manifest
 import android.content.Context
-import android.os.Build
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import com.titoshvily.gpstracker.R
+import androidx.core.content.ContextCompat
+import androidx.core.location.LocationManagerCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 import com.titoshvily.gpstracker.databinding.FragmentMainBinding
-import com.titoshvily.gpstracker.utils.checkPermission
-import com.titoshvily.gpstracker.utils.showToast
+import com.titoshvily.gpstracker.utils.DialogManager
 import org.osmdroid.config.Configuration
 import org.osmdroid.library.BuildConfig
-import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
-
 class MainFragment : Fragment() {
-    private lateinit var pLauncher: ActivityResultLauncher<Array<String>>
-    lateinit var binding: FragmentMainBinding
 
+    private lateinit var binding: FragmentMainBinding
+    private lateinit var requestLocationLauncher: ActivityResultLauncher<Array<String>>
+
+    // --- Жизненный цикл фрагмента ---
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        registerPermissionLauncher()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         settingsOsm()
         binding = FragmentMainBinding.inflate(inflater, container, false)
-        registerPermission()
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        checkLocPermission()
+        registerPermissionLauncher()
+        checkLocationPermission()
     }
 
+    override fun onResume() {
+        super.onResume()
+        checkLocationPermission()
+    }
+
+    // --- Настройка OSM ---
 
     private fun settingsOsm() {
         Configuration.getInstance().load(
-            activity as AppCompatActivity,
-            activity?.getSharedPreferences("osm_pref", Context.MODE_PRIVATE)
+            requireContext(),
+            requireActivity().getSharedPreferences("osm_pref", Context.MODE_PRIVATE)
         )
         Configuration.getInstance().userAgentValue = BuildConfig.APPLICATION_ID
     }
 
     private fun initOsm() = with(binding) {
+
         map.controller.setZoom(20.0)
         val mLocProvider = GpsMyLocationProvider(activity)
-        val mLogOverlay = MyLocationNewOverlay(mLocProvider, map)
-        mLogOverlay.enableMyLocation()
-        mLogOverlay.enableFollowLocation()
-        mLogOverlay.runOnFirstFix {
-            map.overlays.clear()
-            map.overlays.add(mLogOverlay)
+        val mLocOverlay = MyLocationNewOverlay(mLocProvider, map)
+        mLocOverlay.enableMyLocation()
+        mLocOverlay.enableFollowLocation()
+        mLocOverlay.runOnFirstFix {
+                map.overlays.clear()
+                map.overlays.add(mLocOverlay)
         }
+
     }
 
 
-    private fun registerPermission() {
-        pLauncher =
-            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
-                if (it[Manifest.permission.ACCESS_FINE_LOCATION] == true) {
-                    initOsm()
+    private fun registerPermissionLauncher() {
+        requestLocationLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+                // Проверяем, было ли дано основное разрешение
+                if (permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true) {
+                    // Разрешение получено, теперь проверяем GPS и инициализируем карту
+                    checkGpsAndInitMap()
                 } else {
-                    showToast("Отсутствует разрешение местоположения")
+                    // Разрешение не было дано. Показываем объяснение.
+                    showPermissionExplanationDialog("Для работы карты необходимо разрешение на геолокацию.")
                 }
             }
     }
 
 
-    private fun checkLocPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            checkPermissionAfter10()
-        } else {
-            checkPermissionBefore10()
+    private fun checkLocationPermission() {
+
+        when {
+            isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                checkGpsAndInitMap()
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                showPermissionExplanationDialog("Для работы карты нужно разрешение, без него никак.")
+            }
+            else -> {
+                requestLocationLauncher.launch(getPermissionsToRequest())
+            }
         }
     }
 
 
-    @RequiresApi(Build.VERSION_CODES.Q)
-    private fun checkPermissionAfter10() {
-
-        if (checkPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
-            initOsm()
-
-        } else {
-            pLauncher.launch(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION))
-        }
-    }
-
-
-    private fun checkPermissionBefore10() {
-        if (checkPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
+    private fun checkGpsAndInitMap() {
+        if (isLocationEnabled()) {
             initOsm()
         } else {
-            pLauncher.launch(
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
-            )
+            DialogManager.showLocEnableDialog(requireActivity() as AppCompatActivity, object :
+                DialogManager.Listener{
+                override fun onClick() {
+                    startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+                }
+
+            })
         }
     }
+
+
+
+
+    private fun getPermissionsToRequest(): Array<String> {
+        val permissions = mutableListOf<String>()
+        permissions.add(Manifest.permission.ACCESS_FINE_LOCATION)
+
+        return permissions.toTypedArray()
+    }
+
+    // Проверяет, дано ли конкретное разрешение
+    private fun isPermissionGranted(permission: String): Boolean {
+        return ContextCompat.checkSelfPermission(requireContext(), permission) == PackageManager.PERMISSION_GRANTED
+    }
+
+    // Проверяет, включен ли GPS
+    private fun isLocationEnabled(): Boolean {
+        val lm = requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return LocationManagerCompat.isLocationEnabled(lm)
+    }
+
+    // Показывает диалог с объяснением и кнопкой "Настройки"
+    private fun showPermissionExplanationDialog(message: String) {
+        Snackbar.make(binding.root, message, Snackbar.LENGTH_LONG)
+            .setAction("Настройки") {
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package", requireActivity().packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            }
+            .show()
+    }
+
+
 
     companion object {
-
         @JvmStatic
         fun newInstance() = MainFragment()
     }
---
+++ app/src/main/java/com/titoshvily/gpstracker/utils/DialogManager.kt
@@ -0,0 +1,32 @@
+package com.titoshvily.gpstracker.utils
+
+import android.content.Context
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.titoshvily.gpstracker.R
+
+object DialogManager {
+    fun showLocEnableDialog(context: Context, listener: Listener){
+        val builder = AlertDialog.Builder(context)
+        val dialog = builder.create()
+        dialog.setTitle(R.string.location_disabled)
+        dialog.setMessage(context.getString(R.string.location_dialog_message))
+        dialog.setButton(AlertDialog.BUTTON_POSITIVE, "Yes"){
+            _, _ ->
+            listener.onClick()
+
+        }
+        dialog.setButton(AlertDialog.BUTTON_NEGATIVE, "No"){
+                _, _ ->
+            dialog.dismiss()
+
+        }
+        dialog.show()
+    }
+
+
+    interface Listener{
+        fun onClick()
+    }
+
+}
